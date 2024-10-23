65fb5bed52cd3fd979258582e30c11f6df36c6ef
susano-tech <62688887+susano-tech@users.noreply.github.com>
2024-10-23 09:32:28 +0800

    some static page url

--- laravel/resources/views/admin/staticUrls/documents.blade.php
+++ laravel/resources/views/admin/staticUrls/documents.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Баримт бичгүүд</title>
     <style>
         * {
             box-sizing: border-box;
