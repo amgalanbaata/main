3a256e53fc8b737e9f3933be0cf50402fefd84d1
enkhtsogt <m.tumur@susano-tech.com>
2024-10-29 10:03:41 +0800

    user profile url fixed

--- laravel/resources/views/admin/header.blade.php
+++ laravel/resources/views/admin/header.blade.php
@@ -11,7 +11,7 @@
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="icon-user"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 @if (Session::get('admin_is') != 0)
-                <li><a class="dropdown-item" href="user/profile">Профайл</a></li>
+                <li><a class="dropdown-item" href="/admin/user/profile">Профайл</a></li>
                 @endif
                 <li><a class="dropdown-item" href="/admin/logout">Гарах</a></li>
             </ul>
